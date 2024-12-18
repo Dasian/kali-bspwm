12450f264a4a6772fe63e443678cfcedd4ebcdac
Dasian <davidrespiritu99@gmail.com>
2024-12-18 12:35:19 -0500

    Nicer format

--- scripts/Theaming.sh
+++ scripts/Theaming.sh
@@ -48,71 +48,75 @@ echo -e "${Blue} ${White}[${Cyan}i${White}] What type of theme do you want to ap
 
 # added to make the code nicer
 GetTheme() {
-echo ""
-echo -e "${Blue} ${White}[${Cyan}i${White}] Loading themes normal mode..."
-echo ""
-echo -e "${Blue} [${Cyan}1${Blue}] Zenitsu"
-echo -e "${Blue} [${Cyan}2${Blue}] Raven"
-echo -e "${Blue} [${Cyan}3${Blue}] Simon"
-echo -e "${Blue} [${Cyan}4${Blue}] Camila"
-echo -e "${Blue} [${Cyan}5${Blue}] Ryan"
-echo -e "${Blue} [${Cyan}6${Blue}] Esmeralda"
-echo -e "${Blue} [${Cyan}7${Blue}] Xavier"
-echo -e "${Blue} [${Cyan}8${Blue}] Nami"
-echo ""
-echo -ne "${Blue} ▶ ${Red}"
-
-read THEMENAME
-case $THEMENAME in
-	1) THEMENAME="Zenitsu" ;;
-	2) THEMENAME="Raven" ;;
-	3) THEMENAME="Simon" ;;
-	4) THEMENAME="Camila" ;;
-	5) THEMENAME="Ryan" ;;
-	6) THEMENAME="Esmeralda" ;;
-	7) THEMENAME="Xavier" ;;
-	8) THEMENAME="Nami" ;;
-	*)
 	echo ""
-	echo -e "${Blue} ${White}[${Cyan}i${White}] Invalid option, use numbers"
-	sleep 2
-	GetTheme
-	;; 
-esac
+	echo -e "${Blue} ${White}[${Cyan}i${White}] Loading themes normal mode..."
+	echo ""
+	echo -e "${Blue} [${Cyan}1${Blue}] Zenitsu"
+	echo -e "${Blue} [${Cyan}2${Blue}] Raven"
+	echo -e "${Blue} [${Cyan}3${Blue}] Simon"
+	echo -e "${Blue} [${Cyan}4${Blue}] Camila"
+	echo -e "${Blue} [${Cyan}5${Blue}] Ryan"
+	echo -e "${Blue} [${Cyan}6${Blue}] Esmeralda"
+	echo -e "${Blue} [${Cyan}7${Blue}] Xavier"
+	echo -e "${Blue} [${Cyan}8${Blue}] Nami"
+	echo ""
+	echo -ne "${Blue} ▶ ${Red}"
+
+	read THEMENAME
+	case $THEMENAME in
+		1) THEMENAME="Zenitsu" ;;
+		2) THEMENAME="Raven" ;;
+		3) THEMENAME="Simon" ;;
+		4) THEMENAME="Camila" ;;
+		5) THEMENAME="Ryan" ;;
+		6) THEMENAME="Esmeralda" ;;
+		7) THEMENAME="Xavier" ;;
+		8) THEMENAME="Nami" ;;
+		*)
+		echo ""
+		echo -e "${Blue} ${White}[${Cyan}i${White}] Invalid option, use numbers"
+		GetTheme
+		;; 
+	esac
 }
 
 Penetrationthemes () {
-# sets THEMENAME
-GetTheme
+	# sets THEMENAME
+	GetTheme
 
-echo ""
-echo -e " ${White}[${Cyan}i${White}] Loading theme penetration mode ${Red}[${THEMENAME}]${NC}"
+	echo ""
+	echo -e " ${White}[${Cyan}i${White}] Loading theme penetration mode ${Red}[${THEMENAME}]${NC}"
 
-cd ${THEMEDIR}/${THEMENAME}/kitty
-cp color.ini ${CONDIR}/.config/kitty
+	# terminal colors
+	cd ${THEMEDIR}/${THEMENAME}/kitty
+	cp color.ini ${CONDIR}/.config/kitty
 
-# changes window highlight and wallpaper
-cd ${THEMEDIR}/${THEMENAME}
-cp bspwmrc ${CONDIR}/.config/bspwm
+	# bspwm
+	# window highlight and wallpaper
+	cd ${THEMEDIR}/${THEMENAME}
+	cp bspwmrc ${CONDIR}/.config/bspwm
 
-# the colors are cool
-cd ${THEMEDIR}/${THEMENAME}/polybar
-cp user_modules.ini colors.ini ${CONDIR}/.config/polybar/cuts
-cp colors.rasi ${CONDIR}/.config/polybar/cuts/scripts/rofi
+	# polybar and rofi colors
+	cd ${THEMEDIR}/${THEMENAME}/polybar
+	cp user_modules.ini colors.ini ${CONDIR}/.config/polybar/cuts
+	cp colors.rasi ${CONDIR}/.config/polybar/cuts/scripts/rofi
 
-# TODO edit the config.ini structure for ALL themes
-cd ${THEMEDIR}/${THEMENAME}/bar_pentest
-cp config.ini ${CONDIR}/.config/polybar/cuts
+	# TODO edit the config.ini structure for ALL themes
+	cd ${THEMEDIR}/${THEMENAME}/bar_pentest
+	cp config.ini ${CONDIR}/.config/polybar/cuts
 
-cd ${THEMEDIR}/${THEMENAME}/scripts
-cp ethernet_status.sh machine_target.sh vpn_status.sh ${CONDIR}/.config/polybar/cuts/scripts
+	# polybar module scripts (custom colors)
+	cd ${THEMEDIR}/${THEMENAME}/scripts
+	cp ethernet_status.sh machine_target.sh vpn_status.sh ${CONDIR}/.config/polybar/cuts/scripts
 
-echo ""
-betterlockscreen -u ${THEMEDIR}/${THEMENAME}/wallpapers/wal-0.png
-echo ""
-bspc wm -r
-#polybar-msg cmd restart
-echo -e " ${White}[${Cyan}i${White}] ${Red}[${THEMENAME}]${White} theme applied correctly"
-sleep 2
-exit 0
+	# prob a,, better lockscreen
+	echo ""
+	betterlockscreen -u ${THEMEDIR}/${THEMENAME}/wallpapers/wal-0.png
+	echo ""
+
+	# restart window manager
+	bspc wm -r
+
+	echo -e " ${White}[${Cyan}i${White}] ${Red}[${THEMENAME}]${White} theme applied correctly"
+	exit 0
 }
