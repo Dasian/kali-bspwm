22c9ff7852391e17335619e3a4f63def13e6d0b1
Dasian <davidrespiritu99@gmail.com>
2025-05-13 14:40:22 -0400

    Made camila default

--- scripts/wall-scripts/camila_wal.sh
+++ scripts/wall-scripts/camila_wal.sh
@@ -12,5 +12,9 @@ wallpapers=($(find "$wallpapers_dir" -type f -name "*.png" -o -name "*.jpg" -o -
 # RANDOM WALLPAPER
 random_wallpaper="${wallpapers[RANDOM % ${#wallpapers[@]}]}"
 
+# gonna just keep this one
+fixed_wallpaper="${wallpapers_dir}/wal-0.png"
+
 # SET WALLPAPER
-feh --bg-scale "$random_wallpaper"
+# feh --bg-scale "$random_wallpaper"
+feh --bg-scale "$fixed_wallpaper"
